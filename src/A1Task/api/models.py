5bf4b33b86b558e92e182153bf693003f8822467
mixipro <mixipro@yahoo.com>
2023-06-09 12:46:53 +0200

    models

--- src/A1Task/api/models.py
+++ src/A1Task/api/models.py
@@ -5,38 +5,23 @@ from typing import Optional
 from pydantic import BaseModel
 
 
-class User(SQLModel, table=True):
+class CreateUser(SQLModel):
+    gender: str
+    first_name: str
+    last_name: str
+    email: EmailStr
+    username: str
+    password: str
+    birthday: datetime
+    nationality: str
+    cell: str
+    location_street: str
+    location_street_number: int
+    location_city: str
+    location_state: str
+    location_country: str
+    location_postcode: str
+
+
+class User(CreateUser, table=True):
     id: Optional[int] = Field(default=None, primary_key=True)
-    gender: str
-    first_name: str
-    last_name: str
-    email: EmailStr
-    username: str
-    password: str
-    birthday: datetime
-    nationality: str
-    cell: str
-    location_street: str
-    location_street_number: int
-    location_city: str
-    location_state: str
-    location_country: str
-    location_postcode: str
-
-
-class CreateUser(BaseModel):
-    gender: str
-    first_name: str
-    last_name: str
-    email: EmailStr
-    username: str
-    password: str
-    birthday: datetime
-    nationality: str
-    cell: str
-    location_street: str
-    location_street_number: int
-    location_city: str
-    location_state: str
-    location_country: str
-    location_postcode: str
